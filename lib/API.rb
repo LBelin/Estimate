95fc403028e6023ab4c8fba98b5a3afe7cac62e8
Pair1 <pair1@client3.arpcdev.local>
2015-06-17 15:49:36 -0400

    ZM - 22778 - Changed API to an object

--- lib/API.rb
+++ lib/API.rb
@@ -2,16 +2,28 @@ require 'fogbugz'
 
 class API
 
-def self.authenticate
+def authenticate
   token = Rails.application.secrets.token
   fogbugz = Fogbugz::Interface.new(token: token, :uri => 'https://fogbugz.arpcdev.net')
   fogbugz.authenticate
   fogbugz
 end
 
-def self.getAPIinfo(id)
+def getAPIinfo(id)
   fogbugz = self.authenticate
-  @APIinfo = fogbugz.command(:search, q: id, cols: "hrsCurrEst,sTitle,sStatus")
+  bad_info = fogbugz.command(:search, q: id, cols: "hrsCurrEst,sTitle,sStatus")
+  good_info = bad_info.to_s[1..-1]
 end
+
+def initialize(id)
+  @id = id
+  info = self.getAPIinfo(id)
+  @title = info
+  @estimate = info
+end
+
+attr_reader :id
+attr_reader :title
+attr_reader :estimate
 
 end
